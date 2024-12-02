5d3f4fb08d2e7fa0bbcf4439663bd5a1c9947007
batc56 <batc56@hotmail.fr>
2024-12-02 11:24:47 +0100

    MAJ java doc

--- src/Pokemon.java
+++ src/Pokemon.java
@@ -4,6 +4,13 @@ public class Pokemon {
     protected int hp;
     protected int atk;
 
+    /**
+     * Constructeur pour Pokemon
+     * @param nom
+     * @param hp
+     * @param atk
+     */
+
     public Pokemon(String nom, int hp, int atk) {
         this.nom = nom;
         this.hp = hp;
